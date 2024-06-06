f153b9a3ddd5d05bacd554aaa572c4d424a7314a
Martín Amigo <martinamigo@protonmail.com>
2024-06-06 18:29:03 -0300

    paso main de codisAGenis a Main

--- utils/genis-codis-converter/src/Main.java
+++ utils/genis-codis-converter/src/Main.java
@@ -1,5 +1,40 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {// Create the options for the dialog
+        Object[] options = {"De CODIS a GENis", "De GENis a CODIS"};
+
+        // Show the dialog
+        int n = JOptionPane.showOptionDialog(null,
+                "¿Qué conversión quiere realizar?",
+                "Conversión",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
+
+        // Check which option was chosen and show the corresponding form
+        if (n == JOptionPane.YES_OPTION) {
+            codisAGenis();
+        } else if (n == JOptionPane.NO_OPTION) {
+            genisACodis();
+        }
+    }
+
+    private static void codisAGenis() {
+        JFrame frame = new JFrame("Conversor de CODIS a GENis");
+        frame.setContentPane(new codisAGenisForm().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void genisACodis() {
+        JFrame form2 = new JFrame("Conversor de GENis a CODIS");
+        form2.setContentPane(new genisACodisForm().rootPanel);
+        form2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        form2.pack();
+        form2.setVisible(true);
     }
 }
--- utils/genis-codis-converter/src/codisAGenisForm.java
+++ utils/genis-codis-converter/src/codisAGenisForm.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 /*
 TO-DOs:
-- arrancar desde main, no desde este form
 - specimen form: si pone cancel o cerrar que salga del todo
 */
 
@@ -167,44 +166,7 @@ public class codisAGenisForm {
         });
     }
 
-    public static void main(String[] args) {// Create the options for the dialog
-        Object[] options = {"De CODIS a GENis", "De GENis a CODIS"};
-
-        // Show the dialog
-        int n = JOptionPane.showOptionDialog(null,
-                "¿Qué conversión quiere realizar?",
-                "Conversión",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                options[0]);
-
-        // Check which option was chosen and show the corresponding form
-        if (n == JOptionPane.YES_OPTION) {
-            codisAGenis();
-        } else if (n == JOptionPane.NO_OPTION) {
-            genisACodis();
-        }
-    }
-
-    private static void codisAGenis() {
-        JFrame frame = new JFrame("Conversor de CODIS a GENis");
-        frame.setContentPane(new codisAGenisForm().rootPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    private static void genisACodis() {
-        JFrame form2 = new JFrame("Conversor de GENis a CODIS");
-        form2.setContentPane(new genisACodisForm().rootPanel);
-        form2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        form2.pack();
-        form2.setVisible(true);
-    }
-
-    private JPanel rootPanel;
+    public JPanel rootPanel;
     private JLabel nameLabel;
     private JButton convertButton;
     private JTextField userTextField;
