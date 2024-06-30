7c3f462e226f966de66cf24b97ae42eee8ffa0b7
Javier Iserte <jiserte@leloir.org.ar>
2024-06-30 14:07:29 -0300

    [#95] WIP: Modify Forensic Category

--- app/assets/javascripts/modifyForensicCategory/controllers/modifyForensicCategoryController.js
+++ app/assets/javascripts/modifyForensicCategory/controllers/modifyForensicCategoryController.js
@@ -33,12 +33,14 @@ define(
       $scope.models = {
         matchingCodesModel: "",
         newCategory: undefined,
+        currentCategoryName: undefined,
         selectedProfiledata: {}
       };
       $scope.matchingCodes = [];
       $scope.categories = [];
       $scope.stage = 1;
       $scope.confirmedCode = undefined;
+      $scope.profileData = {};
 
       $scope.picturePlaceHolderImage = 'assets/images/default-user.png';
       $scope.inprintPrintPlaceHolderImage = 'assets/images/fingerprint.jpg';
@@ -64,6 +66,7 @@ define(
               $scope.matchingCodes = response.data;
               if ($scope.matchingCodes.length > 0) {
                 $scope.stage = 2;
+                $scope.models.matchingCodesModel = $scope.matchingCodes[0];
               }
             },
             errorMessage
@@ -77,13 +80,12 @@ define(
         // TODO: Check that entered value is noy empty
         $scope.confirmedCode = $scope.models.matchingCodesModel;
         if ($scope.confirmedCode !== undefined) {
+          $scope.models.currentCategoryName = $scope.getCategoryName($scope.confirmedCode.category);
           getProfileData($scope.confirmedCode.globalCode)
             .then(
                function(response) {
                  $scope.models.selectedProfiledata = response.data;
                  $scope.stage = 3;
-                 console.log($scope.models.selectedProfiledata);
-                 console.log($scope.confirmedCode);
                }
             );
         }
@@ -116,18 +118,71 @@ define(
                 .filter(function(x){ return x[1].tipo === 1 ;})
                 .filter(function(x){ return x[1].isReference ;})
                 .map(function(x) {return x[1];});
+              $scope.models.newCategory = $scope.categories[0];
             }
           );
       };
-      $scope.saveProfile = function() {
-        alertService.info("Category Changed!!!");
+
+      $scope.isFieldComplete = function (field) {
+        return field !== undefined && field !== "";
       };
-      $scope.getCagetoryName = function(category) {
+
+      $scope.isObjectDefined = function(object) {
+        for (var property in object){
+          if (object[property] !== undefined && object[property] !== null) {
+            return true;
+          }
+        }
+        return false;
+      };
+
+      var isDataFiliationDefined = function() {
+        return $scope.isObjectDefined($scope.profileData.dataFiliation);
+      };
+
+      var isDataFiliationImages = function() {
+        return (
+          ($scope.pictures[0] !== undefined && $scope.pictures[0] !== $scope.picturePlaceHolderImage) ||
+          ($scope.inprints[0] !== undefined && $scope.inprints[0] !== $scope.inprintPrintPlaceHolderImage) ||
+          ($scope.signatures[0] !== undefined && $scope.signatures[0] !== $scope.signaturePlaceHolderImage)
+        );
+      };
+
+      $scope.saveProfile = function() {
+        var dataFiliationDefined = isDataFiliationDefined();
+        if (dataFiliationDefined) {
+          $scope.profileData.dataFiliation.token = $scope.token;
+        } else {
+          $scope.profileData.dataFiliation = undefined;
+        }
+        var noFiliationData = (!dataFiliationDefined && !isDataFiliationImages());
+
+        // TODO: Check that the logic here is OK, It is replicated from profileDataController.saveProfile method.
+        //       I don't think it works as intended.
+        if (noFiliationData || dataFiliationDefined) {
+          profileDataService
+            .updateProfileData($scope.confirmedCode, $scope.profileData)
+            .then(
+              function (response) {
+                if (response.data) {
+                  alertService.success({message: 'Se ha actualizado el perfil: ' + $scope.confirmedCode});
+                } else {
+                  return Promise.reject("Ha ocurrido un error al actualizar");
+                }
+              }
+            )
+            .catch( function (error) { alertService.error({message: error}); } );
+        } else {
+          alertService.error({message: 'Debe completar todos los datos filiatorios o ninguno'});
+        }
+      };
+
+      $scope.getCategoryName = function(category) {
         var filtered = $scope
           .categories
           .filter(function(x){return x.id === category;});
         if (filtered.length !== 1) {
-          alertService.error("The category "+ category+" is not unique or doesn't exists.");
+          alertService.error({message:"The category " + category + " is not unique or doesn't exists."});
         }
         return filtered[0].name;
       };
