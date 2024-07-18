c12da82bcd8e1938ff875d36434b51abdbe015d5
Javier Iserte <jiserte@leloir.org.ar>
2024-07-18 19:24:41 -0300

    I/110 avglr error (#113)
    * [#110] Fixed an error when there are no markers in profile
    
    * [#110] Fixed an error when there are no markers in profile

--- app/matching/MatchingCalculatorService.scala
+++ app/matching/MatchingCalculatorService.scala
@@ -168,6 +168,8 @@ class MatchingCalculatorServiceImpl @Inject() (
     (result1, result2) match {
       case (Some(r1), Some(r2)) if r1.total > 0 && r2.total > 0 =>
         Some(LRResult((r1.total + r2.total) / 2, avgDetailed(r1.detailed, r2.detailed)))
+      case (Some(r1), Some(r2)) if r1.total == 0 && r1.total == 0 =>
+        None
       case (Some(r1), Some(_)) if r1.total == 0 =>
         result2
       case (Some(_), Some(r2)) if r2.total == 0 =>
