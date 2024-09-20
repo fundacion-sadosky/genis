9a158f5e9e537e1d979c09f78725a3a8ebf196bb
Martín Ignacio Amigo <49733360+tinamigo@users.noreply.github.com>
2024-09-20 17:54:03 -0300

    arreglo orderBy en creación de perfiles
    (en el controlador js)

--- app/assets/javascripts/profiledata/courtCaseDataController.js
+++ app/assets/javascripts/profiledata/courtCaseDataController.js
@@ -3,14 +3,27 @@
  */
 define(['jquery'], function() {
 'use strict';
-	
-function CourtCaseDataController($scope, profileDataService) {
+	function CourtCaseDataController($scope, profileDataService) {
+		profileDataService.getCrimeTypes().then(function(response) {
+			// Almacenar todos los datos de los crímenes
+			$scope.allCrimeData = response.data;
 
-	profileDataService.getCrimeTypes().then(function(response) {
-		$scope.crimeTypes = response.data;
-	});
-	
-}
+			// Extraer los tipos de crímenes como objetos con id y name
+			$scope.crimeTypes = Object.values(response.data).map(function(crimeType) {
+				return { id: crimeType.id, name: crimeType.name };
+			});
+
+			// Vigilar cambios en el tipo de crimen seleccionado
+			$scope.$watch('$parent.profileData.crimeType', function(newVal) {
+				if (newVal && $scope.allCrimeData[newVal]) {
+					// Actualizar la lista de crímenes según el tipo de crimen seleccionado
+					$scope.crimesInvolved = $scope.allCrimeData[newVal].crimes;
+				} else {
+					$scope.crimesInvolved = []; // Reiniciar si no se selecciona un tipo de crimen válido
+				}
+			});
+		});
+	}
 
 return CourtCaseDataController;
 
