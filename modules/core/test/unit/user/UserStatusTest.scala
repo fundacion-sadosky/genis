b1255a8cb195e2905fc0e76d9fb302efb7c93080
M. Janikow (Matata.exe) <matjanikow@gmail.com>
2026-02-27 14:43:52 -0300

    Fix #157 armar test para core
    * Agrego tests unitarios de tipos y algunos de clases de usuario. Arreglo bug encontrado en constrainedText
    
    * Corrijo ruta de login
    
    ---------
    
    Co-authored-by: Martín Amigo <martinamigo@protonmail.com>

---
+++ modules/core/test/unit/user/UserStatusTest.scala
@@ -0,0 +1,59 @@
+package unit.user
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.libs.json.{JsError, JsNumber, JsString, JsSuccess, Json}
+import user.UserStatus
+
+class UserStatusTest extends AnyWordSpec with Matchers:
+
+  "UserStatus" must {
+    "have exactly 5 values" in {
+      UserStatus.values.length mustBe 5
+    }
+
+    "contain active" in {
+      UserStatus.valueOf("active") mustBe UserStatus.active
+    }
+
+    "contain pending" in {
+      UserStatus.valueOf("pending") mustBe UserStatus.pending
+    }
+
+    "contain blocked" in {
+      UserStatus.valueOf("blocked") mustBe UserStatus.blocked
+    }
+
+    "contain inactive" in {
+      UserStatus.valueOf("inactive") mustBe UserStatus.inactive
+    }
+
+    "contain pending_reset" in {
+      UserStatus.valueOf("pending_reset") mustBe UserStatus.pending_reset
+    }
+  }
+
+  "UserStatus JSON Format" must {
+    "serialize active to JsString" in {
+      Json.toJson(UserStatus.active) mustBe JsString("active")
+    }
+
+    "deserialize JsString('blocked') to UserStatus.blocked" in {
+      JsString("blocked").validate[UserStatus] mustBe JsSuccess(UserStatus.blocked)
+    }
+
+    "return JsError for unknown status string" in {
+      JsString("unknown").validate[UserStatus] mustBe a[JsError]
+    }
+
+    "return JsError for non-string JSON" in {
+      JsNumber(1).validate[UserStatus] mustBe a[JsError]
+    }
+
+    "roundtrip all values" in {
+      UserStatus.values.foreach { status =>
+        val json = Json.toJson(status)
+        json.as[UserStatus] mustBe status
+      }
+    }
+  }
