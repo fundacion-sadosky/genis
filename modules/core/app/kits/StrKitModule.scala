61fb5853969d6705594282c52832c4cfe645449d
Martín Amigo <martinamigo@protonmail.com>
2026-03-13 13:56:58 -0300

    WIP #198 migro controlador y dependencias. falta testear

--- modules/core/app/kits/StrKitModule.scala
+++ modules/core/app/kits/StrKitModule.scala
@@ -1,10 +1,14 @@
 package kits
 
 import com.google.inject.AbstractModule
+import pedigree.{MutationService, NoOpMutationService}
 
 class StrKitModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[StrKitRepository]).to(classOf[StrKitRepositoryImpl])
     bind(classOf[StrKitService]).to(classOf[StrKitServiceImpl])
+    bind(classOf[LocusRepository]).to(classOf[LocusRepositoryImpl])
+    bind(classOf[LocusService]).to(classOf[LocusServiceImpl])
+    bind(classOf[MutationService]).to(classOf[NoOpMutationService])
   }
 }
