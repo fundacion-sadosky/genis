e421ce9af17ec2ef677228cc3a94405bd5eb1fbf
M. Janikow (Matata.exe) <matjanikow@gmail.com>
2026-05-28 16:00:00 -0300

    Fix #198 configuracion   locus marcadores v3
    * WIP #198 migro controlador y dependencias. falta testear
    
    * WIP #198 tests para Locus
    
    * Fix #198 migration self-review: fix DI en LocusRepositoryImpl, agregar TODOs para migraciones pendientes
    
    - LocusRepositoryImpl: inyectar Database vía Guice en lugar de Database.forConfig
    - LocusRepositoryImpl: agregar chequeo de existencia en update (fidelidad con legacy)
    - Agregar TODOs: MutationService, AnalysisType, métodos pendientes de LocusService
    - Agregar .envrc al .gitignore
    
    * Fix #198 locus
    
    * Fix #198 compatibilidad de interfaz LocusService/MutationService para merge con new-dev
    
      Implementa saveLocusAllelesFromProfile y refreshAllKis fielmente al legacy:
      saveLocusAllelesFromProfile extrae alelos autosomals del perfil y delega a
      mutationService.saveLocusAlleles; refreshAllKis delega a refreshAllKisSecuential.
      Agrega saveLocusAlleles al trait LocusService. Expande el placeholder MutationService
      con los métodos que consume LocusService y renombra NoOpMutationService a
      MutationServiceStub para alinear la interfaz con la versión completa de new-dev.
    
    ---------
    
    Co-authored-by: Martín Amigo <martinamigo@protonmail.com>

---
+++ modules/core/app/kits/LocusServiceImpl.scala
@@ -0,0 +1,85 @@
+package kits
+
+import javax.inject.{Inject, Singleton}
+import pedigree.MutationService
+import profile.{Allele, Profile}
+import services.{CacheService, LocusCacheKey}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait LocusService:
+  def add(locus: FullLocus): Future[Either[String, String]]
+  def update(locus: FullLocus): Future[Either[String, Unit]]
+  def listFull(): Future[Seq[FullLocus]]
+  def list(): Future[Seq[Locus]]
+  def delete(id: String): Future[Either[String, String]]
+  def getLocusByAnalysisTypeName(analysisType: String): Future[Seq[String]]
+  def getLocusByAnalysisType(analysisType: Int): Future[Seq[String]]
+  def locusRangeMap(): Future[Map[String, AleleRange]]
+  def saveLocusAlleles(list: List[(String, Double)]): Future[Either[String, Int]]
+  def saveLocusAllelesFromProfile(p: Profile): Future[Either[String, Int]]
+  def refreshAllKis(): Future[Unit]
+
+@Singleton
+class LocusServiceImpl @Inject()(
+  cache: CacheService,
+  locusRepository: LocusRepository,
+  mutationService: MutationService
+)(implicit ec: ExecutionContext) extends LocusService:
+
+  private def cleanCache(): Unit =
+    cache.pop(LocusCacheKey)
+
+  override def add(full: FullLocus): Future[Either[String, String]] =
+    locusRepository.add(full).map { result =>
+      result.foreach { _ =>
+        cleanCache()
+        if full.locus.analysisType == 1 then
+          mutationService.addLocus(full)
+      }
+      result
+    }
+
+  override def update(locus: FullLocus): Future[Either[String, Unit]] =
+    locusRepository.update(locus).map { result =>
+      result.foreach(_ => cleanCache())
+      result
+    }
+
+  override def listFull(): Future[Seq[FullLocus]] =
+    cache.asyncGetOrElse(LocusCacheKey)(locusRepository.listFull())
+
+  override def list(): Future[Seq[Locus]] =
+    listFull().map(_.map(_.locus))
+
+  override def delete(id: String): Future[Either[String, String]] =
+    locusRepository.delete(id).map { result =>
+      result.foreach(_ => cleanCache())
+      result
+    }
+
+  override def getLocusByAnalysisTypeName(analysisType: String): Future[Seq[String]] =
+    locusRepository.getLocusByAnalysisTypeName(analysisType).map(_.map(_.id))
+
+  override def getLocusByAnalysisType(analysisType: Int): Future[Seq[String]] =
+    locusRepository.getLocusByAnalysisType(analysisType).map(_.map(_.id))
+
+  override def locusRangeMap(): Future[Map[String, AleleRange]] =
+    list().map { loci =>
+      loci.map(l => l.id -> AleleRange(l.minAlleleValue.getOrElse(0), l.maxAlleleValue.getOrElse(99))).toMap
+    }
+
+  override def saveLocusAlleles(list: List[(String, Double)]): Future[Either[String, Int]] =
+    mutationService.saveLocusAlleles(list)
+
+  override def saveLocusAllelesFromProfile(profile: Profile): Future[Either[String, Int]] =
+    val locusAlleles = profile.genotypification.get(1)
+      .map(_.toList.flatMap { case (marker, alleles) =>
+        alleles.collect { case Allele(z) => (marker.toString, z.toDouble) }
+      })
+      .getOrElse(List.empty)
+      .filter(_._1.nonEmpty)
+    saveLocusAlleles(locusAlleles)
+
+  override def refreshAllKis(): Future[Unit] =
+    mutationService.refreshAllKisSecuential()
